0e12f7e98ac115d11265fe149b1b0316f6902269
philipDmartin <philipnathan788@gmail.com>
2020-12-25 00:25:16 -0600

    added purchaseInventory

---
+++ VendingMachineCsharp/Controllers/PurchaseTransactionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using VendingMachineCsharp.Data;
+using VendingMachineCsharp.Repositories;
+using VendingMachineCsharp;
+
+namespace VendingMachineCsharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseTransactionsController : ControllerBase
+    {
+        private readonly PurchaseTransactionsRepository _purchaseTransactionsRepository;
+
+        public PurchaseTransactionsController(ApplicationDbContext context)
+        {
+            _purchaseTransactionsRepository = new PurchaseTransactionsRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_purchaseTransactionsRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var purchaseTransactions = _purchaseTransactionsRepository.GetById(id);
+            if (purchaseTransactions == null)
+            {
+                return NotFound();
+            }
+            return Ok(purchaseTransactions);
+        }
+    }
+}
