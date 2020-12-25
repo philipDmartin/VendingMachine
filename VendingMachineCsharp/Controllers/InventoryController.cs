9c1c90b77530179391c9c1cb23e7a20394b3cd74
philipDmartin <philipnathan788@gmail.com>
2020-12-25 00:16:41 -0600

    added repo and controller for inventory

---
+++ VendingMachineCsharp/Controllers/InventoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using VendingMachineCsharp.Data;
+using VendingMachineCsharp.Repositories;
+using VendingMachineCsharp;
+
+namespace VendingMachineCsharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly InventoryRepository _inventoryRepository;
+
+        public InventoryController(ApplicationDbContext context)
+        {
+            _inventoryRepository = new InventoryRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_inventoryRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var inventory = _inventoryRepository.GetById(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+            return Ok(inventory);
+        }
+    }
+}
--- VendingMachineCsharp/Repositories/ProductRepository.cs
+++ VendingMachineCsharp/Repositories/ProductRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using VendingMachineCsharp.Data;
+using VendingMachineCsharp;
 
 namespace VendingMachineCsharp.Repositories
 {
-    class ProductRepository
+    public class ProductRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _context.Product.ToList();
+        } 
+
+        public Product GetById(int id)
+        {
+            return _context.Product.FirstOrDefault(p => p.Id == id);
+        }
     }
 }
